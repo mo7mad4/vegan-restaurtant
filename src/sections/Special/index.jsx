91de7a6cdf7c8a46bc59e82e892a7dcb88180788
mo7mad4 <mohammedreda814@gmail.com>
2023-07-01 20:19:03 +0300

    making map function inside Special section

--- src/sections/Special/index.jsx
+++ src/sections/Special/index.jsx
@@ -1,42 +1,31 @@
-import React, { Component } from 'react'
-import CardComponentWithSpecial from '../../components/CardComponentWithSpecial'
-import GridContant from '../../components/GridContant'
-import ThreeCardForSpecial from './ThreeCardForSpecial'
-import orangeJuice from '../../images/special/orange-juice.png';
-import chef from '../../images/special/chef.png';
-import restaurant from '../../images/special/restaurant.png';
-import './style.css'
+import React, { Component } from "react";
+import CardComponentWithSpecial from "../../components/CardComponentWithSpecial";
+import GridContant from "../../components/GridContant";
+import ThreeCardForSpecial from "./ThreeCardForSpecial";
+import specialData from "../../mock/SpecialData";
+
+import "./style.css";
 
 class Special extends Component {
   render() {
     return (
-        <div className="special">
-            <GridContant
-                title="special"
-                subTitle="What makes us special"
-                isCentered
+      <div className="special">
+        <GridContant
+          title="special"
+          subTitle="What makes us special"
+          isCentered
+        />
+        <CardComponentWithSpecial>
+          {specialData.map((data) => (
+            <ThreeCardForSpecial
+              img={data.img}
+              title={data.title}
+              content={data.content}
             />
-            <CardComponentWithSpecial>
-                <ThreeCardForSpecial
-                    img={orangeJuice}
-                    title="Fresh food"
-                    content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras."
-                />
-                <ThreeCardForSpecial
-                    img={chef}
-                    title="skilled Chef"
-                    content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras."
-                />
-                <ThreeCardForSpecial
-                    img={restaurant}
-                    title="Exotic dishes"
-                    content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras."
-                />
-            </CardComponentWithSpecial>
-
-        </div>
-
-    )
+          ))}
+        </CardComponentWithSpecial>
+      </div>
+    );
   }
 }
-export default Special
+export default Special;
