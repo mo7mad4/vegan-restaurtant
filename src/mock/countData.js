daf21b24a78307c4d004a265b2f52549c5cdd526
mo7mad4 <mohammedreda814@gmail.com>
2023-07-01 20:24:16 +0300

    Create countData mock data

---
+++ src/mock/countData.js
@@ -0,0 +1,22 @@
+import dishImage from '../images/Numbers/dish.png'
+import chefImage from '../images/Numbers/chef.png'
+import teamImage from '../images/Numbers/team.png'
+
+const countData = [
+    {
+        img:dishImage,
+        number:"250+",
+        title:"Delicacy"
+    },
+    {
+        img:chefImage,
+        number:"10+",
+        title:"renowed chefs"
+    },
+    {
+        img:teamImage,
+        number:"30+",
+        title:"Members"
+    }
+]
+export default countData
